f197eeb405d030fb825f757d6f9bcb7e5e01e783
Luciana Romano <luciana.romano@est.fi.uncoma.edu.ar>
2025-06-04 18:00:48 -0300

    🎨

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<<<<<<< HEAD
         <title>My post</title>
-=======
-        <title>Laravel</title>
->>>>>>> fb9b7b4f8b1e7b85af59b982b81479b9ee8720e3
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -19,7 +15,6 @@
             @vite(['resources/css/app.css', 'resources/js/app.js'])
         @endif
     </head>
-<<<<<<< HEAD
     <body class="bg-[#ffffff] dark:bg-[#E7F2E4] text-[#ffffff] min-h-screen">
         <header class="fixed top-0 left-0 w-full z-50 bg-[#FDFDFC] dark:bg-[#732255] shadow text-[#E7F2E4]">
             <div class="max-w-6xl mx-auto flex justify-between items-center py-4 px-6">
@@ -93,44 +88,5 @@
                 </div>
             </main>
         </div>
-=======
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
-            @if (Route::has('login'))
-                <nav class="flex items-center justify-end gap-4">
-                    @auth
-                        <a
-                            href="{{ route('categories.index') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
-                        >
-                            posts.index
-                        </a>
-                    @else
-                        <a
-                            href="{{ route('login') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
-                        >
-                            Log in
-                        </a>
-
-                        @if (Route::has('register'))
-                            <a
-                                href="{{ route('register') }}"
-                                class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
-                                Register
-                            </a>
-                        @endif
-                    @endauth
-                </nav>
-            @endif
-        </header>
-        <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
-            <h1>ESTAS EN HOME</h1>
-        </div>
-
-        @if (Route::has('login'))
-            <div class="h-14.5 hidden lg:block"></div>
-        @endif
->>>>>>> fb9b7b4f8b1e7b85af59b982b81479b9ee8720e3
     </body>
 </html>
