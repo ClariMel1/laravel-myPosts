9b8cda22f98f98c10697034824eba49806ec33fe
Clara Pelozo <clara.pelozo@est.fi.uncoma.edu.ar>
2025-06-06 16:01:12 -0300

    🎨 se diseño una bonita filtracion de categorias con un scroll y la posibilidad de agregar categorias

--- resources/views/category/edit.blade.php
+++ resources/views/category/edit.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+    <x-header-categories />
     <div class="mx-auto max-w-4xl py-5">
     <div class="max-w-4xl mx-auto px-0 flex flex-col md:flex-row rounded shadow overflow-hidden" style="min-height: 400px;">
         <form action="{{ route('categories.update', $category->slug) }}" method="POST" class="bg-[#1e1e1e] w-full px-12 py-8 overflow-auto transition-all duration-300" id="formSection">
