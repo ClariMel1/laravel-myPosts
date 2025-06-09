4cae77d9a4a421a50952f75e3f31d0678a21725f
Luciana Romano <luciana.romano@est.fi.uncoma.edu.ar>
2025-06-08 23:00:47 -0300

    ✨Agrege paginacion y le di estilos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,10 @@
     <x-main>
         <x-title> {{ __('Posts más recientes') }}</x-title>
         <x-lista-posts :posts="$posts" />
+    
+    <!--paginacion-->
+    <div class="pagination">
+            {{ $posts->appends(request()->query())->links() }}
+        </div>
     </x-main>
-
 </x-app-layout>
